90281df98e57f41e711e556bb16b8b59e68fdd03
terrymuleja <thierrynsaka@gmail.com>
2025-08-16 21:41:10 +0200

    cleanup db

---
+++ Denudey.Api.Services.Infrastructure/Migrations/ApplicationDb/20250816193856_CleanupDbXXXXXXXXXXXXXX.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Denudey.Api.Services.Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class CleanupDbXXXXXXXXXXXXXX : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "ProductViews");
+            migrationBuilder.DropTable(name: "ProductLikes");
+            migrationBuilder.DropTable(name: "EpisodeViews");
+            migrationBuilder.DropTable(name: "EpisodeLikes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
